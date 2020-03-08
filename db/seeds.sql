534106cd95b0b375295953729e25b3c20af5b7d4
chylinski.t <chylinski.t@outlook.com>
2020-03-08 18:55:29 -0400

    pre updated version

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,3 @@
-
--- create a `seeds.sql` file. In this file, write insert queries to populate the `burgers` table with at least three entries.
+INSERT INTO burger (burger_name, devoured) values ('TMacBurger', 0);
+INSERT INTO burger (burger_name, devoured) values ('MacBurger', 0);
+INSERT INTO burger (burger_name, devoured) values ('TRexBurger', 0);
