70e1c24e5c9156b6401980caadf932ba44bbc7b8
chylinski.t <chylinski.t@outlook.com>
2020-03-14 23:05:03 -0400

    updated server.js file

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require("express");
+var exhbs = require("express-handlebars");
+//var bodyParser = require("body-parser");
+
 
 var PORT = process.env.PORT || 8080;
-
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
@@ -17,11 +19,8 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-
-//var routes = require("./controllers/catsController.js");
-
-//app.use(routes);
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes)
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
